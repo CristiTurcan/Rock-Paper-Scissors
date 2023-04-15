3aee1766253eafbf97750187e7fb2eb240f52d46
CristiTurcan <turcancristian02@gmail.com>
2023-04-15 23:26:49 +0300

    A game is 5 rounds and added score

--- script.js
+++ script.js
@@ -14,43 +14,64 @@ function getPlayerChoice() {
 function playRound(playerSelection, computerSelection) {
     if(!choices.includes(playerSelection)) {
         alert("Nu ai introdus datele corecte");
-        return;
+        return 3;
     }
     
     if(playerSelection === "ROCK") {
         if(computerSelection === "ROCK") {
             alert("Egal");
-            return;
+            return 0;
         } else if(computerSelection === "PAPER") {
             alert("Ai pierdut! Hartia bate Piatra");
-            return;
+            return 1;
         } else if(computerSelection === "SCISSORS") {
             alert("Ai castigat! Piatra bate Foarfecele!");
-            return;
+            return 2;
         }
     } else if(playerSelection === "PAPER") {
         if(computerSelection === "PAPER") {
             alert("Egal");
-            return;
+            return 0;
         } else if(computerSelection === "SCISSORS") {
             alert("Ai pierdut! Foarfecele bate Hartia");
-            return;
+            return 1;
         } else if(computerSelection === "ROCK") {
             alert("Ai castigat! Hartia bate Piatra!");
-            return;
+            return 2;
         }
     } else if(playerSelection === "SCISSORS") {
         if(computerSelection === "SCISSORS") {
             alert("Egal");
-            return;
+            return 0;
         } else if(computerSelection === "ROCK") {
             alert("Ai pierdut! Piatra bate Hartia");
-            return;
+            return 1;
         } else if(computerSelection === "PAPER") {
             alert("Ai castigat! Foarfecele bate Piatra!");
-            return;
+            return 2;
         }
     }
 }
 
-playRound(getPlayerChoice(), getComputerChoice());
+function game() {
+    let computerScore = 0;
+    let playerScore = 0;
+    let winner = 0;
+
+    for(let i = 0; i < 5; i++){
+        winner = playRound(getPlayerChoice(), getComputerChoice());
+
+        if(winner === 1){
+            computerScore++;
+            alert(`Calulator - Player: ${computerScore} - ${playerScore}`);
+        } else if (winner === 2) {
+            playerScore++;
+        }
+
+        alert(`Calulator - Player: ${computerScore} - ${playerScore}`);
+    }
+
+    alert(`Meciul s-a terminat! Scor final:\nCalculator - Player: ${computerScore} - ${playerScore}`)
+}
+
+game();
